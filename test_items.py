65f5da99ade873618c8e49a32fa5b59c3d97c396
Alexandr <107554672+ShAhTeR67@users.noreply.github.com>
2022-09-14 21:10:43 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,4 +1,3 @@
-import time
 from selenium.webdriver.common.by import By
 
 link = r' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
@@ -6,7 +5,6 @@ link = r' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_add_to_basket_btn_exists(browser):
     browser.get(link)
-    time.sleep(15)
     button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
     assert button, f"Button wasn't found!"
 
