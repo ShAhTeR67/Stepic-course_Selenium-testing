30391af8bc0126755cacbad73ff5d26bb4fa8061
Alexsmaznov@gmail.com <Alexsmaznov@gmail.com>
2022-09-14 21:24:19 +0300

    Refactored test logic.

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ link = r' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_add_to_basket_btn_exists(browser):
     browser.get(link)
-    button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    button = browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket')
     assert button, f"Button wasn't found!"
